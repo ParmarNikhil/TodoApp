d7bf52b6bf9d7a4ac81833a4538ae82d8c724d6a
ParmarNikhil <np0598029@gmail.com>
2021-07-23 13:56:37 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,108 @@
+import { Component } from 'react';
+import './App.css';
+import './Todo.css';
+import shortid from 'shortid';
+
+class App extends Component {
+  state = {
+    temp: "",
+    tododata: [],
+    completed: [],
+    whichtoshow: ""
+  }
+
+  handlechange = (event) => {
+    event.preventDefault();
+    this.setState({ temp: event.target.value });
+  }
+
+  addToList = () => {
+    var today = new Date();
+    this.setState({ tododata: [{ "id": shortid.generate(), "text": this.state.temp, "date": today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate() + '-' + today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds(), "iscomplete": false }, ...this.state.tododata] })
+    console.log(this.state.tododata);
+    
+    this.setState({ temp: "" });
+  }
+
+  removeTodo = (todo) => {
+    const cpdata = this.state.tododata;
+    cpdata.splice(cpdata.indexOf(todo),1);
+    this.setState({tododata:cpdata});
+  }
+
+  clearAll = () => {
+    this.setState({ tododata: [] });
+  }
+  addToComplete = (todo) => {
+    const today = new Date();
+    let data = this.state.tododata.map((t)=>{
+      if(t.id===todo.id){
+        return {...todo,"id":todo.id,"iscomplete":true,"date": today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate() + '-' + today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds()}
+      }
+      else{
+        return t;
+      }
+      
+    })
+    
+    console.log(data);
+    this.setState({tododata:data});
+    // this.setState({tododata:[{"iscomplete":true,...todo},...this.state.tododata]})
+    // const cpdata = this.state.tododata
+    // this.setState({tododata:cpdata})
+  }
+
+  
+
+  render() {
+    let tododata = [];
+    if (this.state.whichtoshow === "completed") {
+      tododata = this.state.tododata.filter((
+        todo => todo.iscomplete
+      ))
+    }
+    else if(this.state.whichtoshow==="active") {
+      tododata = this.state.tododata.filter((          
+        todo => !todo.iscomplete
+      ))
+    }
+    else{
+      tododata = this.state.tododata;
+    }
+    return (
+      <body>
+      <div className="maincontainer">
+
+        <header>Todo List</header>
+        <div className="inputbox">
+        <input type="text" onChange={this.handlechange} value={this.state.temp} placeholder="add something" />
+        </div>
+        <div className="buttonlist">
+        <button onClick={this.addToList}>add</button>&nbsp;
+        <button onClick={this.clearAll}>clear all</button>&nbsp;
+        <button onClick={() => this.setState({ whichtoshow: "completed" })}>completed</button>&nbsp;
+        <button onClick={() => this.setState({ whichtoshow: "active" })}>active todos</button>
+        </div>
+        <br></br>
+        <div className="todolist">
+
+          {tododata.map((todo) =>
+            <div className="todoitems" key={todo.id}>Task: {todo.text} 
+            <br></br>
+            Time:  &nbsp; {todo.date} &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <button onClick={()=>this.removeTodo(todo)}>✗</button>
+          &nbsp;
+            {!todo.iscomplete ?
+            <button onClick={() => this.addToComplete(todo)} id="completebtn" className="cmpbtn" >✓</button>
+            :null
+            }  
+              
+            </div>
+          )}
+        </div><br></br>
+        </div>
+        </body>
+    );
+  }
+}
+
+export default App;
